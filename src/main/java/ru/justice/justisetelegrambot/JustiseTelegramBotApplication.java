43bedb76123e83962f05e3fb2c4cd4d4cd483aa4
Ilyas Abdullabekov <IAbdullabekov@croc.ru>
2023-03-28 21:54:23 +0300

    init commit

---
+++ src/main/java/ru/justice/justisetelegrambot/JustiseTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.justice.justisetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustiseTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustiseTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/justice/justisetelegrambot/JustiseTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.justice.justisetelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustiseTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
